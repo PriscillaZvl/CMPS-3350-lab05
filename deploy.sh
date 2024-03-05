7bbce76c291e159d39da346b4187ed392c72ea68
Priscilla Zavala <pzavala@odin.cs.csubak.edu>
2024-03-04 17:41:46 -0800

    Included audios and font in the deploy.sh script file

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DEST=~/public_html/3350/lab05
-declare -a FILES=("game.html" "level.js")
+declare -a FILES=("game.html" "level.js" "audio/cartoon-jump-6462.mp3" "audio/collision.mp3" "fonts/Kubasta.ttf")
 mkdir -p $DEST
 for i in "${FILES[@]}"
 do
